396ca5910b379f30e3e8da36b08e7e781981b68f
Harshad <Harshad@DESKTOP-LGI0VO1>
2020-07-14 14:23:05 +0530

    first commit

---
+++ sentiment-analysis/src/main/java/com/degree/project/model/SentimentResult.java
@@ -0,0 +1,28 @@
+package com.degree.project.model;
+
+public class SentimentResult {
+	
+	double sentimentScore;
+	String sentimentType;
+	SentimentClassification sentimentClass;
+	
+	public double getSentimentScore() {
+		return sentimentScore;
+	}
+	public void setSentimentScore(double sentimentScore) {
+		this.sentimentScore = sentimentScore;
+	}
+	public String getSentimentType() {
+		return sentimentType;
+	}
+	public void setSentimentType(String sentimentType) {
+		this.sentimentType = sentimentType;
+	}
+	public SentimentClassification getSentimentClass() {
+		return sentimentClass;
+	}
+	public void setSentimentClass(SentimentClassification sentimentClass) {
+		this.sentimentClass = sentimentClass;
+	}
+
+}
